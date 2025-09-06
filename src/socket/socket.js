27ab3f659b4f2d2cd777ec0c66ba8ffd944c830a
Priyatriathi <priyatripathi0415@gmail.com>
2025-09-06 16:40:51 +0530

    frontend5

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", {
+const socket = io("https://split-backend-3.onrender.com" || "http://localhost:5000", {
   withCredentials: true,
   transports: ["websocket"],
   autoConnect: true,
